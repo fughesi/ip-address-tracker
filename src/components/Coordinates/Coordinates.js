3ba37179153c9660e0f108fdecdc394df97757cb
Steven <steven.camillieri@protonmail.com>
2022-02-23 10:20:55 -0500

    API

--- src/components/Coordinates/Coordinates.js
+++ src/components/Coordinates/Coordinates.js
@@ -1,8 +1,45 @@
-import React from 'react'
-import "./Coordinates.css"
+import React, { useState, useEffect } from "react";
+import "./Coordinates.css";
 
 export default function Coordinates() {
+  // state variables
+  const [IP, setIP] = useState("Loading...");
+  const [location, setLocation] = useState("Loading...");
+  const [timezone, setTimezone] = useState("Loading...");
+  const [ISP, setISP] = useState("Loading...");
+
+  //API call
+  useEffect(() => {
+    fetch(
+      "https://geo.ipify.org/api/v2/country,city?apiKey=at_wqE4QzgMbHPp0sxYo8gSNrE0JbDvX&ipAddress=8.8.8.8"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setIP(data.ip);
+        setLocation([data.location.region, data.location.country, data.location.postalCode]);
+        setTimezone(data.location.timezone);
+        setISP(data.isp);
+      });
+  }, []);
+
   return (
-    <div>Coordinates</div>
-  )
+    <main id="coordinateMain">
+      <div className="IP">
+        <p>IP ADDRESS</p>
+        <h3>{IP}</h3>
+      </div>
+      <div className="location">
+        <p>LOCATION</p>
+        <h3>{`${location[0]}, ${location[1]} ${location[2]}`}</h3>
+      </div>
+      <div className="timezone">
+        <p>TIMEZONE</p>
+        <h3>{`UTC ${timezone}`}</h3>
+      </div>
+      <div className="ISP">
+        <p>ISP</p>
+        <h3>{ISP}</h3>
+      </div>
+    </main>
+  );
 }
--- src/images/Arrow/Arrow.jsx
+++ src/images/Arrow/Arrow.jsx
@@ -6,7 +6,7 @@ export default function Arrow() {
   return (
     <div>
       <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14">
-        <path fill="none" stroke="#FFF" stroke-width="3" d="M2 1l6 6-6 6" />
+        <path fill="none" stroke="#FFF" strokeWidth="3" d="M2 1l6 6-6 6" />
       </svg>
     </div>
   );
