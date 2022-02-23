49816de5aa946ea4f7862d03ea682984b2f0c549
Steven <steven.camillieri@protonmail.com>
2022-02-23 02:31:47 -0500

    components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Coordinates from './components/Coordinates/Coordinates';
+import Map from './components/Map/Map';
+import Search from './components/Search/Search';
+import TopFrame from './components/TopFrame/TopFrame';
 
 function App() {
   return (
     <div className="AppWrap">
-      
+      <TopFrame />
+      <Coordinates />
+      <Map />
+      <Search />
     </div>
   );
 }
